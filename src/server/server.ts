da4c104e5b0b3c2275e31230f519d74527344ec6
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-11-26 06:51:41 -0300

    feat: initial commit

---
+++ src/server/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { Response } from 'express';
+
+const server = express();
+
+server.get('/teste', (_, response: Response) => {
+    return response.send('Hello World!');
+});
+
+export { server };
